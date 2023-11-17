dc0b25829491bf1eaf524055d9326a29d8893e6e
jinukrishnan <jinukrishnan.p@gmail.com>
2023-11-17 17:12:30 +0530

    sasa

--- Static page/staticProj/staticProj/settings.py
+++ Static page/staticProj/staticProj/settings.py
@@ -122,4 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIR=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'statics')]
+# STATIC_ROOT=[os.path.join(BASE_DIR,'assets')]
+# MEDIA_URL='/media/'
+# MEDIA_ROOT=[os.path.join(BASE_DIR,'media')]
+
