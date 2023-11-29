9e5270716a9416b51acf6f081a059a825a30ccae
jinukrishnan <jinukrishnan.p@gmail.com>
2023-11-29 13:20:59 +0530

    updated

--- movieProj/movieProj/settings.py
+++ movieProj/movieProj/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATTIC_ROOT=os.path.join(BASE_DIR,'assets')
+MEDIA_URL='media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
